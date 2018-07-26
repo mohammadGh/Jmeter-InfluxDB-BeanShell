cd0a9e8995b5b28b07807d5abc0a5f17dd777c8a
mgh <m.ghoreshi@gmail.com>
2018-07-26 18:55:30 +0430

    some fields and tags are revised

--- beanShell_toInfluxDB.java
+++ beanShell_toInfluxDB.java
@@ -5,33 +5,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.util.EntityUtils;
 
-//prepare string for request
-result = new StringBuilder();
-
-status = "Failure";
-if(sampleResult.isSuccessful())
-status = "Success";
-
-result.append("samples,")
-.append("tag=")
-.append("${myTag}")
-.append(",label=")
-.append(sampleResult.getSampleLabel())
-.append(",status=")
-.append(status)
-.append(" ")
-.append("count=")
-.append(sampleResult.getSampleCount())
-.append(",threads=")
-.append(sampleResult.getAllThreads())
-.append(",duration=")
-.append(sampleResult.getTime())
-.append(",responsecode=")
-.append(sampleResult.getResponseCode())
-.append( " ")
-.append(sampleResult.getTimeStamp())
-.append("000000");
-
 //Escape the string values before posting the data
 String escapeValue(String val){
 val = val.replaceAll(",", "\\\\,")
@@ -45,11 +18,54 @@ return val;
 void PostMeasurement(String metric){
 httpclient = new DefaultHttpClient(new BasicHttpParams());
 httpPost = new HttpPost(); 
-httpPost.setURI(new URI("http://localhost:8086/write?db=vtm_jmeter"));
+httpPost.setURI(new URI("http://localhost:8086/write?db=isna"));
 httpPost.setEntity(new StringEntity(metric));
 HttpResponse response = httpclient.execute(httpPost);
 EntityUtils.consumeQuietly(response.getEntity());
 }
+
+//prepare string for request
+result = new StringBuilder();
+
+status = "Failure";
+if(sampleResult.isSuccessful())
+status = "Success";
+
+result.append("samples,")
+.append("tag=")
+.append("${myTag}")
+.append(",label=")
+.append(sampleResult.getSampleLabel())
+.append(",thread=")
+.append(sampleResult.getThreadName().replace(" ","_"))
+.append(",status=")
+.append(status)
+.append(" ")
+.append("count=")
+.append(sampleResult.getSampleCount())
+.append(",threads=")
+.append(sampleResult.getAllThreads())
+.append(",load_time=")
+.append(sampleResult.getTime())
+.append(",latency_time=")
+.append(sampleResult.getLatency())
+.append(",connect_time=")
+.append(sampleResult.getConnectTime())
+.append(",idle_time=")
+.append(sampleResult.getIdleTime())
+.append(",start_time=")
+.append(sampleResult.getStartTime())
+.append(",end_time=")
+.append(sampleResult.getEndTime())
+.append(",responsecode=")
+.append(sampleResult.getResponseCode())
+.append(",recieve=")
+.append(sampleResult.getBytesAsLong())
+.append(",send=")
+.append(sampleResult.getSentBytes())
+.append( " ")
+.append(sampleResult.getTimeStamp())
+.append("000000");
 
 //To call the function which posts the data
 PostMeasurement(result.toString());
